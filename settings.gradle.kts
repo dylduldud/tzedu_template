be55dc8fe05cc57e43164f2e17705537aaab2960
GitHub Action <action@github.com>
2021-10-06 06:51:12 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "tzedu_template"
--- src/main/kotlin/com/github/dylduldud/tzedutemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dylduldud/tzedutemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dylduldud.tzedutemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dylduldud.tzedutemplate.MyBundle
 
 class MyApplicationService {
 
